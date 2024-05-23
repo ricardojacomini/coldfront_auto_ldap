0484121237a121a4c435a1e4d7fde47b089750c9
Jeffrey Condell <jdcrc@rit.edu>
2024-05-23 11:53:18 -0400

    set up signals and stubbed tasks

--- coldfront_plugin_auto_ldap/signals.py
+++ coldfront_plugin_auto_ldap/signals.py
@@ -2,4 +2,22 @@ import logging
 from django.dispatch import receiver
 from django_q.tasks import async_task
 
+from coldfront.core.allocation.signals import allocation_activate, allocation_activate_user, allocation_remove_user
+from coldfront.core.project.views import ProjectAddUsersView, ProjectRemoveUsersView
+
 logger = logging.getLogger(__name__)
+
+@receiver(allocation_activate)
+def add_group(sender, **kwargs):
+    allocation_pk = kwargs.get('allocation_pk')
+    async_task('coldfront_plugin_auto_ldap.tasks.add_group', allocation_pk)
+
+@receiver(allocation_activate_user, sender=ProjectAddUsersView)
+def add_user(sender, **kwargs):
+    allocation_user_pk = kwargs.get('allocation_user_pk')
+    async_task('coldfront_plugin_auto_ldap.tasks.add_user', allocation_user_pk)
+
+@receiver(allocation_remove_user, sender=ProjectRemoveUsersView)
+def remove_user(sender, **kwargs):
+    allocation_user_pk = kwargs.get('allocation_user_pk')
+    async_task('coldfront_plugin_auto_ldap.tasks.remove_user', allocation_user_pk)
--- coldfront_plugin_auto_ldap/tasks.py
+++ coldfront_plugin_auto_ldap/tasks.py
@@ -2,5 +2,15 @@ import logging
 from ldap3 import Server, Connection, TLS, get_config_parameter, set_config_parameter, SASL
 
 from coldfront.core.utils.common import import_from_settings
+from coldfront.core.allocation.models import Allocation, AllocationUser
 
 logger = logging.getLogger(__name__)
+
+def add_group(allocation_pk):
+    pass
+
+def add_user(allocation_user_pk):
+    pass
+
+def remove_user(allocation_user_pk):
+    pass
