da8e3d9e66f3c0c310afc15b847f1774b4f55269
Jeffrey Condell <jdcrc@rit.edu>
2024-05-28 14:12:02 -0400

    added user and project searching, creation, and deletion to utils

--- coldfront_plugin_auto_ldap/utils.py
+++ coldfront_plugin_auto_ldap/utils.py
@@ -1,4 +1,5 @@
 import logging
+from ldap3 import Server, Connection, TLS, get_config_parameter, set_config_parameter, SASL, ALL
 
 from coldfront.core.utils.common import import_from_settings
 
@@ -44,3 +45,59 @@ def parse_uri(uri):
     for part in partURI:
         parsed += ',dc=' + part
     return parsed
+
+def search_project(conn, project):
+    search_base = 'cn=' + project + uri # this probably needs fixing
+    search_scope = 'base'
+    search_filter = '(objectClass=*)'
+    try:
+        conn.search(search_base=search_base,
+                    search_filter=search_filter,
+                    search_scope=search_scope) # this probably needs fixing
+        results = connection.entries
+    except LDAPException as e:
+        resutls = e
+
+def add_project(conn, project):
+    try:
+        response = conn.add('cn=' + project + ",ou=coldfront" + uri, ['groupOfNames', 'top']) #this probably needs fixing
+    except LDAPException as e:
+        logger.warn(e)
+
+def search_user(conn, username):
+    search_base = uri # this probably needs fixing
+    search_scope = SUBTREE
+    search_filter = '(uid=' + username + ')'
+    try:
+        conn.search(search_base=search_base,
+                    search_filter=search_filter,
+                    search_scope=search_scope)
+        results = connection.entries
+    except LDAPException as e:
+        results = e
+
+def add_user(conn, username):
+    try:
+        response = conn.add('uid=' + username + ",ou=coldfront", + uri, ['inetOrgPerson', 'top']) #this probably needs fixing
+    except LDAPException as e:
+        logger.warn(e)
+
+def add_user_group(conn, username, group):
+    search_project(conn, project)
+
+    if len(conn.entries) != 0:
+        try:
+            conn.modify('cn=' + project + ",ou=coldfront" + uri, {'member': [(MODIFY_ADD, ['uid=' + username + ",ou=coldfront"])]}) #this probably needs fixing
+        except LDAPException as e:
+            results = e
+            return -1
+
+def remove_user_group(conn, username):
+    search_project(conn, project)
+
+    if len(conn.entries) != 0:
+        try:
+            conn.modify('cn=' + project + ",ou=coldfront" + uri, {'member': [(MODIFY_DELETE, ['uid=' + username + ",ou=coldfront"])]}) #this probably needs fixing
+        except LDAPException as e:
+            results = e
+            return -1
