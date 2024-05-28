9acfab3fe48752a4772cb177dfecf9c3fce8d197
Jeffrey Condell <jdcrc@rit.edu>
2024-05-28 13:34:23 -0400

    set up command to manually add users and groups

---
+++ coldfront_plugin_auto_ldap/management/commands/ldap_add.py
@@ -0,0 +1,44 @@
+import logging
+
+from django.core.management.base import BaseCommand, CommandError
+
+from coldfront.core.project.models import Project, ProjectAttribute
+from coldfront.core.user.models import User, UserAtrribute
+
+from coldfront_plugin_auto_ldap.tasks import add_group, add_user
+
+logger = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    help = "Adds users or projects to LDAP"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-u",
+            "--user",
+            help="Add a user, use -p to specify a project to add the user to"
+        )
+        parser.add_argument(
+            "-p",
+            "--project",
+            help="Add a project, or specify project when used with -u"
+        )
+
+    def handle(self, *args, **options):
+        projects = Allocation.objects.all()
+        users = User.objects.all()
+
+        user = options["user"]
+        project = options["project"]
+
+        if user != None:
+            if project != None:
+                pass
+            pass
+        elif project:
+            pass
+        else:
+            for p in projects:
+                pass
+            for u in users:
+                pass
